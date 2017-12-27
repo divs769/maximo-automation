a8ab4ee8becf8cfaa887f5b42dbcf6f3362ece15
Pessatto Queiroz <gyowanny.pessattoqueiroz@shopdirect.com>
2017-12-27 10:46:55 +0000

    initial commit

--- src/main/java/com/shopdirect/microservicetemplate/ServiceApplication.java
+++ src/main/java/com/shopdirect/microservicetemplate/ServiceApplication.java
@@ -1,10 +1,35 @@
 package com.shopdirect.microservicetemplate;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication(scanBasePackages = { "com.shopdirect.version", "com.shopdirect.microservicetemplate" })
 public class ServiceApplication {
+
+	@Bean
+	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+		return new Jackson2ObjectMapperBuilder() {
+			@Override
+			public void configure(ObjectMapper objectMapper) {
+				super.configure(objectMapper);
+				objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+				objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+				objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+				objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+				objectMapper.setNodeFactory(JsonNodeFactory.withExactBigDecimals(true));
+			}
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
