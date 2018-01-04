652ac399104b271481e37a5db951a61ce55004ba
Pessatto Queiroz <gyowanny.pessattoqueiroz@shopdirect.com>
2018-01-04 15:52:12 +0000

    WIP jenkins integration

--- src/main/java/com/shopdirect/maximoautomation/infrastructure/RethinkDBConnectionFactory.java
+++ src/main/java/com/shopdirect/maximoautomation/infrastructure/RethinkDBConnectionFactory.java
@@ -2,22 +2,31 @@ package com.shopdirect.maximoautomation.infrastructure;
 
 import com.rethinkdb.RethinkDB;
 import com.rethinkdb.net.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
 
+import static com.shopdirect.maximoautomation.infrastructure.DBInitializer.MAXIMO_DB;
+
 @Component
 public class RethinkDBConnectionFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RethinkDBConnectionFactory.class);
 
-    @Value("${host.key:localhost}")
+    @Value("${db.host:localhost}")
     private String host;
+    @Value("${db.port:28015}")
+    private int port;
 
     public Connection createConnection() {
-        return RethinkDB.r.connection().hostname(host).connect();
+        LOGGER.info("Connecting to database at {}:{}", host, port);
+        return RethinkDB.r.connection().hostname(host).port(port).connect();
     }
 
     public Connection connectToMaximoDb() {
-        return RethinkDB.r.connection().hostname(host).db(DBInitializer.MAXIMO_DB).connect();
+        LOGGER.info("Connecting to database at {}:{}", host, port);
+        return RethinkDB.r.connection().hostname(host).port(port).db(MAXIMO_DB).connect();
     }
 }
