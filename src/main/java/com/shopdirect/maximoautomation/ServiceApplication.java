28b3e9eca6904bf2c424052a176e1078c35773c1
Pessatto Queiroz <gyowanny.pessattoqueiroz@shopdirect.com>
2018-01-09 12:27:20 +0000

    Adds logging incoming requests support

--- src/main/java/com/shopdirect/maximoautomation/ServiceApplication.java
+++ src/main/java/com/shopdirect/maximoautomation/ServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -32,6 +33,16 @@ public class ServiceApplication {
 			}
 
 		};
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+		return loggingFilter;
 	}
 
 	public static void main(String[] args) {
