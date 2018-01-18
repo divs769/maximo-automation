1bb1c1347e7114ddad32423d5986e21630317ae7
gyowanny.pessattoqueiroz@shopdirect.com <gyowanny.pessattoqueiroz@shopdirect.com>
2018-01-18 15:42:33 +0000

    Adds unit tests for the validation service. Changes the format the errors are returned when converted from List to String

--- src/main/java/com/shopdirect/maximoautomation/infrastructure/service/ValidationService.java
+++ src/main/java/com/shopdirect/maximoautomation/infrastructure/service/ValidationService.java
@@ -9,12 +9,11 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 public class ValidationService {
-
     private static final Pattern PATTERN = Pattern.compile("(https?://)?[\\w.]+(:\\d+)?/job/[\\w-]+/\\d+/$");
-
     private final BuildInfoDao buildInfoDao;
 
     @Autowired
@@ -25,18 +24,18 @@ public class ValidationService {
     public List<String> validateBuildStarted(BuildInfo buildInfo) {
         List<String> errors = new ArrayList<>(checkInvalidTime(buildInfo.getStartTime()));
         if(buildInfo.getBuildId() == null) {
-            errors.add("Missing build ID.");
+            errors.add("Missing build ID");
         }
         if(buildInfo.getUrl() == null) {
-            errors.add("Missing URL.");
+            errors.add("Missing URL");
         } else if(!PATTERN.matcher(buildInfo.getUrl()).matches()) {
-            errors.add("Invalid URL.");
+            errors.add("Invalid URL");
         }
         if(buildInfo.getVcTag() == null) {
-            errors.add("Missing tag.");
+            errors.add("Missing tag");
         }
         if(buildInfo.getVcDescription() == null) {
-            errors.add("Missing description.");
+            errors.add("Missing description");
         }
         return errors;
     }
@@ -44,17 +43,17 @@ public class ValidationService {
     public List<String> updateValidations(BuildInfo buildInfo) {
         List<String> errors = new ArrayList<>(checkInvalidTime(buildInfo.getFinishTime()));
         if(buildInfo.getId() == null) {
-            errors.add("Build ID is missing.");
+            errors.add("Build ID is missing");
             return errors;
         }
         if(buildInfo.getStatus() == null) {
-            errors.add("Status is missing.");
+            errors.add("Status is missing");
         }
         BuildInfo existingRecord = buildInfoDao.getRecord(buildInfo.getId());
         if(existingRecord == null) {
-            errors.add("Record doesn't exist in the database.");
+            errors.add("Record doesn't exist in the database");
         } else if(buildInfo.getFinishTime() != null && buildInfo.getFinishTime().isBefore(existingRecord.getStartTime())) {
-            errors.add("Finish date should be after start date.");
+            errors.add("Finish date should be after start date");
         }
         return errors;
     }
@@ -62,18 +61,14 @@ public class ValidationService {
     public List<String> checkInvalidTime(OffsetDateTime time) {
         List<String> errors = new ArrayList<>();
         if(time == null) {
-            errors.add("Missing time.");
+            errors.add("Missing time");
         } else if(time.isAfter(OffsetDateTime.now())) {
-            errors.add("Invalid time.");
+            errors.add("Invalid time");
         }
         return errors;
     }
 
     public static String generateErrorString(List<String> errors) {
-        StringBuilder sb = new StringBuilder();
-        for(String error : errors) {
-            sb.append(error).append("\n");
-        }
-        return sb.toString();
+        return errors.stream().collect(Collectors.joining(","));
     }
 }
---
+++ src/test/java/com/shopdirect/maximoautomation/infrastructure/service/ValidationServiceTest.java
@@ -0,0 +1,151 @@
+package com.shopdirect.maximoautomation.infrastructure.service;
+
+import com.shopdirect.maximoautomation.infrastructure.dao.BuildInfoDao;
+import com.shopdirect.maximoautomation.infrastructure.resource.BuildInfo;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import static com.shopdirect.maximoautomation.infrastructure.resource.BuildStatus.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class ValidationServiceTest {
+    @MockBean
+    private BuildInfoDao buildInfoDao;
+    private ValidationService instance;
+
+    @Before
+    public void setUp() throws Exception {
+        instance = new ValidationService(buildInfoDao);
+    }
+
+    @Test
+    public void shouldValidBuildStartedInfoAndReturnAnEmptyListWithouErrors() {
+        // Given
+        BuildInfo buildInfo = createValidBuildInfo();
+
+        // When
+        List<String> errors = instance.validateBuildStarted(buildInfo);
+
+        // Then
+        assertThat(errors).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnErrorsWhenBuildStartedInfoDoesNotContainAnyData() {
+        // Given
+        BuildInfo buildInfo = mock(BuildInfo.class);
+
+        // When
+        List<String> errors = instance.validateBuildStarted(buildInfo);
+
+        // Then
+        assertThat(errors).isNotEmpty();
+        assertThat(errors).containsExactly(
+                "Missing time",
+                "Missing build ID",
+                "Missing URL",
+                "Missing tag",
+                "Missing description");
+    }
+
+    @Test
+    public void shouldReturnErrorsForInvalidUrlAndInvalidTimeForBuildStartedValidation() {
+        // Given
+        BuildInfo buildInfo = mock(BuildInfo.class);
+        OffsetDateTime invalidDateTime = OffsetDateTime.now().plusHours(2);
+        when(buildInfo.getStartTime()).thenReturn(invalidDateTime);
+        when(buildInfo.getUrl()).thenReturn("http://invalid");
+        when(buildInfo.getBuildId()).thenReturn("123");
+        when(buildInfo.getVcTag()).thenReturn("tag");
+        when(buildInfo.getVcDescription()).thenReturn("description");
+
+        // When
+        List<String> errors = instance.validateBuildStarted(buildInfo);
+
+        // Then
+        assertThat(errors).isNotEmpty();
+        assertThat(errors).containsExactly(
+                "Invalid time",
+                "Invalid URL");
+    }
+
+    @Test
+    public void shouldValidateAndReturnNoErrorsForAValidUpdateValidations() {
+        // Given
+        BuildInfo buildInfo = mock(BuildInfo.class);
+        when(buildInfo.getId()).thenReturn("id");
+        when(buildInfo.getStatus()).thenReturn(SUCCESS);
+        OffsetDateTime startDateTime = OffsetDateTime.now().minusHours(2);
+        when(buildInfo.getStartTime()).thenReturn(startDateTime);
+        when(buildInfo.getFinishTime()).thenReturn(startDateTime.plusHours(1));
+
+        when(buildInfoDao.getRecord(any(String.class))).thenReturn(buildInfo);
+
+        // When
+        List<String> errors = instance.updateValidations(buildInfo);
+
+        // Then
+        assertThat(errors).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnErrorWhenRecordDoesNotExistInDatabaseForUpdateValidation() {
+        // Given
+        BuildInfo buildInfo = mock(BuildInfo.class);
+        when(buildInfo.getId()).thenReturn("id");
+        when(buildInfo.getStatus()).thenReturn(SUCCESS);
+        when(buildInfo.getFinishTime()).thenReturn(OffsetDateTime.now().minusHours(1));
+
+        when(buildInfoDao.getRecord(any(String.class))).thenReturn(null);
+
+        // When
+        List<String> errors = instance.updateValidations(buildInfo);
+
+        // Then
+        assertThat(errors).containsExactly("Record doesn't exist in the database");
+    }
+
+    @Test
+    public void shouldReturnErrorWhenFinishDateIsBeforeStartDateWhenUpdatingValidation() {
+        // Given
+        BuildInfo buildInfo = mock(BuildInfo.class);
+        when(buildInfo.getId()).thenReturn("id");
+        when(buildInfo.getStatus()).thenReturn(SUCCESS);
+        OffsetDateTime startDateTime = OffsetDateTime.now().minusHours(2);
+        when(buildInfo.getStartTime()).thenReturn(startDateTime);
+        when(buildInfo.getFinishTime()).thenReturn(startDateTime.minusHours(1));
+
+        when(buildInfoDao.getRecord(any(String.class))).thenReturn(buildInfo);
+
+        // When
+        List<String> errors = instance.updateValidations(buildInfo);
+
+        // Then
+        assertThat(errors).containsExactly("Finish date should be after start date");
+    }
+
+    private BuildInfo createValidBuildInfo() {
+        return new BuildInfo(
+                    "11111",
+                    "123",
+                    "https://host:8080/job/project/123/",
+                    OffsetDateTime.now(),
+                    OffsetDateTime.now().plusMinutes(2),
+                    "hash",
+                    "tag",
+                    "branch",
+                    "description",
+                    SUCCESS
+            );
+    }
+}
